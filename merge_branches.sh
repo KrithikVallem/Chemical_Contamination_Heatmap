49e8732055994911d6abdf312f529edc52ec6e74
vallemkr <vallemkr@umich.edu>
2020-11-13 18:13:38 -0500

    Added a script to merge master and gh-pages branches

---
+++ merge_branches.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script merges the master branch (where code is pushed to) with the gh-pages branch (which is what the live website uses)
+# I got this from https://gist.github.com/mandiwise/44d1edce18f2ffb14f63
+
+# This script can be run using the terminal command below, make sure the path is correct relative to your current terminal location, 
+# and make sure you use the right filename (if this file is not called merge_branches.sh, then change that part of the command):
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# bash merge_branches.sh
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Also note that you can provide an argument, for your git commit message:
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# bash merge_branches.sh "This is my commit message!"
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Reference: http://lea.verou.me/2011/10/easily-keep-gh-pages-in-sync-with-master/
+git add .
+git status # to see what changes are going to be commited
+git commit -m "$1" # $1 is the first command line argument (bash merge_branches.sh "My Commit Message") -> My Commit Message" is $1
+git push origin master
+: '
+git checkout gh-pages # go to the gh-pages branch
+git rebase master # bring gh-pages up to date with master
+git push origin gh-pages # commit the changes
+git checkout master # return to the master branch
+'
