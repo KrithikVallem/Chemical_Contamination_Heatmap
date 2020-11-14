bc96628c68b92a8f357bb165c72b59ba5eb97342
vallemkr <vallemkr@umich.edu>
2020-11-14 13:29:20 -0500

    some more changes

--- README.md
+++ README.md
@@ -5,10 +5,25 @@ A website to easily let users view heatmaps of the contamination levels of vario
 We obtained the data for this website by gathering and analyzing 400,000+ publicly available unstructured EPA datapoints. You can check out the repo for that portion at https://github.com/UMEcoData/Michigan-Chemical-Contamination-Heatmaps! 
 
 **Things we still have to do:**
-* Get Descriptions for all chemicals, and update the ones we have currently
-* Update the README.md file to indicate the web user tool is finished
-* Add in the about page stuff (including header link!!!) and put everyone's faces and descriptions onto it
-* Figure out if there are units for the chemical contamination values, and add those in to heatmap and zipcode summary modal
+* Get Descriptions for all chemicals that don't have one
+    * 1-butanol
+    * 2-methoxyethanol
+    * chromium
+    * germanium
+    * HAA5
+    * HAA6Br
+    * HAA9
+    * NDMA
+    * oxyfluorfen
+    * testosterone
+* Possibly edit other chemical descriptions to remove/modify the numbers that indicate which source certain info was from, and update the demo image afterwards once done
 * Look over and finalize the main project description - Note: Some of the technical details behind how the website and heatmap works are misleading, so fix if needed - Heatmap_Tech_Details.txt
 * Add a link to the live website on the EcoData website, at the very top of the project description <--- VERY IMPORTANT
-* Delete more unneeded files to clean up repo (_config.yml and index.md and Heatmap_Tech_Details??)
+---
+Stuff that can be done after meeting, but need an opinion on during meeting:
+* : Update the README.md file to remove this todo section
+* Add in the about page stuff (including header link!!!) and put everyone's faces and descriptions onto it
+* : Figure out if there are units for the chemical contamination values, and add those in to heatmap and zipcode summary modal
+* : Possibly limit the length of chemical contamination values (10.0633333333) -> (10.063) ?? - on both heatmap and zipcode modal
+* : Delete more unneeded files to clean up repo (_config.yml and index.md and Heatmap_Tech_Details??)
+* : have the main ecodata github fork this repo and launch their own github pages site, and update all links afterwards
---
+++ about/generate_about_page_html.js
@@ -0,0 +1,85 @@
+// This script takes in an array of objects representing each team member, and spits out html that should go in about.html
+// run script like so:
+// node [this file's name/path, without the brackets] > [output file name, without the brackets]
+// make sure you've npm installed the needed libraries
+
+const inputFileFormat = "csv"; // change to "json" if you have json
+const inputFilePath = ""; // use the correct path here relative to this file
+function renameProperties(json) {
+    // rename the properties to match what the html template uses
+    json.forEach(person => {
+        return {
+            // these below are examples, don't actually use these below:
+            image: person.headshot,
+            name,
+            description: person.shortDescription + person.interests,
+        }
+    })
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+const fs = require('fs');
+const parse = require('csv-parse/lib/sync');
+
+main();
+
+function generateHTML(peopleJSON) {
+    
+    let template = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title> About Us </title>
+        <link rel="stylesheet" type="text/css" href="chem_test_style.css">
+    </head>
+    <body>
+    <!--Add nav_bar or just back button?-->
+        <div class="about_hdr">
+            <h1> About The Members </h1>
+            <p> Short summary about our work </p>
+    <!---This page should be responsive--->
+        </div>
+    `;
+
+    template += `<div class="row">`
+
+    for (const person of peopleJSON) {
+        template += `
+            <div class="column">
+                <div class="card">
+                    <img src="${person.image}" alt="${person.name}" style="width: 100%">
+                    <div class="container_about">
+                        <h2>${person.name}</h2>
+                        <p class="title">${person.gradYearOrClassStanding} in ${person.majorAndMinor}</p>
+                        <p>${person.description}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+    }
+
+    template += `
+        </div>
+    </body>
+    `;
+}
+
+function main() {
+    let fileContent = fs.readFileSync(inputFilePath, "utf-8");
+
+    // convert csv to json
+    if (inputFileFormat === "csv") {
+        fileContent = parse(fileContent, { columns: true });
+    }
+    else if (inputFileFormat === "json") {
+        fileContent = JSON.parse(fileContent);
+    }
+    else {
+        console.log(`Unknown File Format Used: ${inputFileFormat}`);
+    }
+
+    fileContent = renameProperties(fileContent);
+    generateHTML(fileContent);
+}
