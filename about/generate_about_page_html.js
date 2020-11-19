dee595b40d8cf077dcee3287e0d09cb85782b7a6
vallemkr <vallemkr@umich.edu>
2020-11-19 02:52:03 -0500

    added other people to about.html, but don't have access to images yet and the page needs more formatting ;; Stuff to do in December meetings: *Finish the about page, including restoring the responsiveness on mobile devices   *fixing project description tech overview stuff   *Fork into main ecodata repo and start new gh pages and change all links to it

--- about/generate_about_page_html.js
+++ about/generate_about_page_html.js
@@ -7,16 +7,19 @@
 const inputFilePath = `${__dirname}/Chemical Heatmap About Page Submissions .csv`; // use the correct path here relative to your terminal location
 function renameProperties(json) {
     // rename the properties to match what the html template uses, the original property names are from the google form questions
-    return json.map(person => ({
+    return json.map(person => (
+        {
             name: person["Name "],
             gradYearOrClassStanding: person['Graduating Year and/or Standing (ex. Class of 2023 or Soph.)'],
             majorAndMinor: person['Major and/or Minors (if applicable)'],
             image: person["Headshot "],
             workedOn: person['Short Description of what you worked on in the project '],
             interests: person["Short description of what you're involved in/interested in (ex. Neural Networks, Machines learning, sustainability practices, renewable energy, etc.) "],
-        })
-    );
+        }
+    ));
 }
+
+const contributorsWhoDidntFillOutGoogleForm = ["Jiyang Zhou", "Lily Zhai", "Adam Mentzer", "Madeline Richards", "Dhanuj Gandikota", "Karl Goddard", "Daniel Schenider", "Keerthi Sajja", "Michael Yufa-Zimilevich"];
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -28,7 +31,7 @@ const { dirname } = require('path');
 main();
 
 function generateHTML(peopleJSON) {
-    
+    // boilerplate stuff at the top
     let template = `
         <!DOCTYPE html>
             <html lang="en">
@@ -69,6 +72,7 @@ function generateHTML(peopleJSON) {
 		</div>
     `
 
+    // generate card for each person who filled out google form
     for (const person of peopleJSON) {
         template += `
             <div class="column">
@@ -76,7 +80,7 @@ function generateHTML(peopleJSON) {
                     <img src="${person.image}" alt="${person.name}" style="width: 100%">
                     <div class="container_about">
                         <h2>${person.name}</h2>
-                        <p class="title">${person.gradYearOrClassStanding}, ${person.majorAndMinor}</p>
+                        <p class="title">${person.gradYearOrClassStanding}<br>${person.majorAndMinor}</p>
                         <u>What I worked on:</u>
                             <p>${person.workedOn}</p>
                         <u>What I'm interested in:</u>
@@ -87,8 +91,26 @@ function generateHTML(peopleJSON) {
             `;
     }
 
+    template += `</div>
+
+    `;
+
+
+    // generate basic list for people who didn't fill out google form
     template += `
-        </div>
+        <div>
+            <strong>Other Contributors:</strong>
+            <ul>
+                ${
+                contributorsWhoDidntFillOutGoogleForm
+                    .map(name => `<li>${name}</li>`)
+                    .join(`\n`)
+                }
+            </ul>
+        </div>`;
+
+
+    template += `
       </body>
     </html>
     `;
